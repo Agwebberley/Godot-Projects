a4ed4174f90d470b76ad369d0594610bce08eb97
Andrew Webberley <44350096+Agwebberley@users.noreply.github.com>
2023-01-23 16:08:29 -0800

    housekeeping

---
+++ Pong/Ball.gd
@@ -0,0 +1,15 @@
+extends RigidBody2D
+
+@export var GoalLeft: Node
+@export var GoalRight: Node
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+func _on_body_entered(body):
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
